5479289571ee802f9a8f1e940bcfcd013c74aba9
Cédric Compagnon <cedric.compagnon@outlook.fr>
2017-06-05 17:34:55 +0200

    itération 5

--- app/app.php
+++ app/app.php
@@ -9,8 +9,20 @@ ExceptionHandler::register();
 
 // Register service providers.
 $app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+$app->register(new Silex\Provider\AssetServiceProvider(), array(
+    'assets.version' => 'v1'
+));
+
 
 // Register services.
 $app['dao.article'] = function ($app) {
     return new projet4\DAO\ArticleDAO($app['db']);
+};
+$app['dao.comment'] = function ($app) {
+    $commentDAO = new projet4\DAO\CommentDAO($app['db']);
+    $commentDAO->setArticleDAO($app['dao.article']);
+    return $commentDAO;
 };
