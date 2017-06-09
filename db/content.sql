0e15bdffc0262342999ae1d12afb4e6a213e2c80
Cédric Compagnon <cedric.compagnon@outlook.fr>
2017-06-09 21:45:08 +0200

    Structure Base de données

--- db/content.sql
+++ db/content.sql
@@ -1,21 +1,33 @@
 insert into t_episode values
-(1, 'First episode', 'Hi there! This is the very first episode.');
+(1, 'Premier épisode', '<p>Ibique intersaepientes in Isauriae itinera viatorum adventicium inveniretur et opibus igitur pascebantur maritima ibique opibus et tempore Lycaoniam maritima pascebantur contulerunt Isauriae praetenturis in se itinera contulerunt opibus densis inveniretur ora opibus ora et Lycaoniam inveniretur intersaepientes se igitur nihil opibus Isauriae ibique densis ora cum in densis mox praetenturis contulerunt adnexam opibus Lycaoniam relicta adventicium cum et mox maritima ibique nihil adnexam contulerunt Lycaoniam itinera tempore densis Isauriae se ibique adventicium nihil maritima Lycaoniam igitur praetenturis ora igitur ibique tempore pascebantur intersaepientes cum in nihil se relicta adventicium adventicium intersaepientes nihil relicta Lycaoniam itinera ibique intersaepientes adnexam adventicium ora.</p>', NOW());
 insert into t_episode values
-(2, 'Lorem ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit mauris ac porttitor accumsan. Nunc vitae pulvinar odio, auctor interdum dolor. Aenean sodales dui quis metus iaculis, hendrerit vulputate lorem vestibulum. Suspendisse pulvinar, purus at euismod semper, nulla orci pulvinar massa, ac placerat nisi urna eu tellus. Fusce dapibus rutrum diam et dictum. Sed tellus ipsum, ullamcorper at consectetur vitae, gravida vel sem. Vestibulum pellentesque tortor et elit posuere vulputate. Sed et volutpat nunc. Praesent nec accumsan nisi, in hendrerit nibh. In ipsum mi, fermentum et eleifend eget, eleifend vitae libero. Phasellus in magna tempor diam consequat posuere eu eget urna. Fusce varius nulla dolor, vel semper dui accumsan vitae. Sed eget risus neque.');
+(2, 'Second épisode', '<p>Ibique intersaepientes in Isauriae itinera viatorum adventicium inveniretur et opibus igitur pascebantur maritima ibique opibus et tempore Lycaoniam maritima pascebantur contulerunt Isauriae praetenturis in se itinera contulerunt opibus densis inveniretur ora opibus ora et Lycaoniam inveniretur intersaepientes se igitur nihil opibus Isauriae ibique densis ora cum in densis mox praetenturis contulerunt adnexam opibus Lycaoniam relicta adventicium cum et mox maritima ibique nihil adnexam contulerunt Lycaoniam itinera tempore densis Isauriae se ibique adventicium nihil maritima Lycaoniam igitur praetenturis ora igitur ibique tempore pascebantur intersaepientes cum in nihil se relicta adventicium adventicium intersaepientes nihil relicta Lycaoniam itinera ibique intersaepientes adnexam adventicium ora.</p>', NOW());
 insert into t_episode values
-(3, 'Lorem ipsum in french', "J’en dis autant de ceux qui, par mollesse d’esprit, c’est-à-dire par la crainte de la peine et de la douleur, manquent aux devoirs de la vie. Et il est très facile de rendre raison de ce que j’avance. Car, lorsque nous sommes tout à fait libres, et que rien ne nous empêche de faire ce qui peut nous donner le plus de plaisir, nous pouvons nous livrer entièrement à la volupté et chasser toute sorte de douleur ; mais, dans les temps destinés aux devoirs de la société ou à la nécessité des affaires, souvent il faut faire divorce avec la volupté, et ne se point refuser à la peine. La règle que suit en cela un homme sage, c’est de renoncer à de légères voluptés pour en avoir de plus grandes, et de savoir supporter des douleurs légères pour en éviter de plus fâcheuses.");
+(3, 'Troisième épisode', '<p>Ibique intersaepientes in Isauriae itinera viatorum adventicium inveniretur et opibus igitur pascebantur maritima ibique opibus et tempore Lycaoniam maritima pascebantur contulerunt Isauriae praetenturis in se itinera contulerunt opibus densis inveniretur ora opibus ora et Lycaoniam inveniretur intersaepientes se igitur nihil opibus Isauriae ibique densis ora cum in densis mox praetenturis contulerunt adnexam opibus Lycaoniam relicta adventicium cum et mox maritima ibique nihil adnexam contulerunt Lycaoniam itinera tempore densis Isauriae se ibique adventicium nihil maritima Lycaoniam igitur praetenturis ora igitur ibique tempore pascebantur intersaepientes cum in nihil se relicta adventicium adventicium intersaepientes nihil relicta Lycaoniam itinera ibique intersaepientes adnexam adventicium ora.</p>', NOW());
+insert into t_episode values
+(4, 'Quatrième épisode', '<p>Ibique intersaepientes in Isauriae itinera viatorum adventicium inveniretur et opibus igitur pascebantur maritima ibique opibus et tempore Lycaoniam maritima pascebantur contulerunt Isauriae praetenturis in se itinera contulerunt opibus densis inveniretur ora opibus ora et Lycaoniam inveniretur intersaepientes se igitur nihil opibus Isauriae ibique densis ora cum in densis mox praetenturis contulerunt adnexam opibus Lycaoniam relicta adventicium cum et mox maritima ibique nihil adnexam contulerunt Lycaoniam itinera tempore densis Isauriae se ibique adventicium nihil maritima Lycaoniam igitur praetenturis ora igitur ibique tempore pascebantur intersaepientes cum in nihil se relicta adventicium adventicium intersaepientes nihil relicta Lycaoniam itinera ibique intersaepientes adnexam adventicium ora.</p>', NOW());
+insert into t_episode values
+(5, 'Cinquième épisode', '<p>Ibique intersaepientes in Isauriae itinera viatorum adventicium inveniretur et opibus igitur pascebantur maritima ibique opibus et tempore Lycaoniam maritima pascebantur contulerunt Isauriae praetenturis in se itinera contulerunt opibus densis inveniretur ora opibus ora et Lycaoniam inveniretur intersaepientes se igitur nihil opibus Isauriae ibique densis ora cum in densis mox praetenturis contulerunt adnexam opibus Lycaoniam relicta adventicium cum et mox maritima ibique nihil adnexam contulerunt Lycaoniam itinera tempore densis Isauriae se ibique adventicium nihil maritima Lycaoniam igitur praetenturis ora igitur ibique tempore pascebantur intersaepientes cum in nihil se relicta adventicium adventicium intersaepientes nihil relicta Lycaoniam itinera ibique intersaepientes adnexam adventicium ora.</p>', NOW());
 
 /* raw password is 'john' */
 insert into t_user values
-(1, 'JohnDoe', '$2y$13$F9v8pl5u5WMrCorP9MLyJeyIsOLj.0/xqKd/hqa5440kyeB7FQ8te', 'YcM=A$nsYzkyeDVjEUa7W9K', 'ROLE_USER');
+(1, 'JohnDoe', 'testJohn@live.fr', '$2y$13$F9v8pl5u5WMrCorP9MLyJeyIsOLj.0/xqKd/hqa5440kyeB7FQ8te', 'YcM=A$nsYzkyeDVjEUa7W9K', 'ROLE_USER');
 /* raw password is 'jane' */
 insert into t_user values
-(2, 'JaneDoe', '$2y$13$qOvvtnceX.TjmiFn4c4vFe.hYlIVXHSPHfInEG21D99QZ6/LM70xa', 'dhMTBkzwDKxnD;4KNs,4ENy', 'ROLE_USER');
+(2, 'JaneDoe', 'testJane@live.fr', '$2y$13$qOvvtnceX.TjmiFn4c4vFe.hYlIVXHSPHfInEG21D99QZ6/LM70xa', 'dhMTBkzwDKxnD;4KNs,4ENy', 'ROLE_USER');
 /* raw password is '@dm1n' */
 insert into t_user values
-(3, 'admin', '$2y$13$A8MQM2ZNOi99EW.ML7srhOJsCaybSbexAj/0yXrJs4gQ/2BqMMW2K', 'EDDsl&fBCJB|a5XUtAlnQN8', 'ROLE_ADMIN');
+(3, 'Jean Forteroche', 'admin@live.fr', '$2y$13$A8MQM2ZNOi99EW.ML7srhOJsCaybSbexAj/0yXrJs4gQ/2BqMMW2K', 'EDDsl&fBCJB|a5XUtAlnQN8', 'ROLE_ADMIN');
 
 insert into t_comment values
-(1, 'Great! Keep up the good work','https://openclassrooms.com','test@test.fr', 1, 1);
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac.', 1, 1, NOW());
 insert into t_comment values
-(2, "Thank you, I'll try my best.",'https://openclassrooms.com','test@test.fr', 1, 2);
+(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac.', 1, 2, NOW());
+insert into t_comment values
+(3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac.', 2, 1, NOW());
+insert into t_comment values
+(4, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac.', 2, 2, NOW());
+insert into t_comment values
+(5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac.', 3, 1, NOW());
+insert into t_comment values
+(6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac.', 3, 2, NOW());
--- db/structure.sql
+++ db/structure.sql
@@ -5,12 +5,14 @@ drop table if exists t_episode;
 create table t_episode (
     art_id integer not null primary key auto_increment,
     art_title varchar(100) not null,
-    art_content varchar(2000) not null
+    art_content TEXT,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
 create table t_user (
     usr_id integer not null primary key auto_increment,
     usr_name varchar(50) not null,
+    usr_email varchar(50) not null,
     usr_password varchar(88) not null,
     usr_salt varchar(23) not null,
     usr_role varchar(50) not null 
@@ -19,10 +21,9 @@ create table t_user (
 create table t_comment (
     com_id integer not null primary key auto_increment,
     com_content varchar(500) not null,
-    com_url varchar(500) null,
-    com_email varchar(200) not null,
     art_id integer not null,
     usr_id integer not null,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     constraint fk_com_art foreign key(art_id) references t_episode(art_id),
     constraint fk_com_usr foreign key(usr_id) references t_user(usr_id)
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
