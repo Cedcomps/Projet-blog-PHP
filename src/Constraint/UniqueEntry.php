8c15f88f8ab333eea9702ccde61eb605845479cb
Cédric Compagnon <cedric.compagnon@outlook.fr>
2017-06-11 20:59:27 +0200

    Essai Constraint Validator

---
+++ src/Constraint/UniqueEntry.php
@@ -0,0 +1,16 @@
+<?php
+namespace projet4\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+class Unique extends Constraint 
+{
+    public $notUniqueMessage = '%string% est déjà utilisé.';
+    public $entity;
+    public $field;
+
+    public function validatedBy()
+    {
+        return 'validator.unique';
+    }
+}
---
+++ src/Constraint/UniqueEntryValidator.php
@@ -0,0 +1,24 @@
+<?php
+namespace projet4\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class UniqueEntryValidator extends ConstraintValidator 
+{
+    private $app;
+
+    public function __construct($app) {
+        $this->app = $app;
+    }
+    public function validate($value, Constraint $constraint)
+    {
+        $exists = $this->app['dao.' . $constraint->entity ]->findOneBy(array($constraint->field, $value));
+        if ($exists)
+        {
+            $this->context->addViolation($constraint->notUniqueMessage, array('%string%' => $value));
+            return false;
+        }
+        return true;
+    }
+}
